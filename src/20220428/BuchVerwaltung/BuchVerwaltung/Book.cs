830ca91795ce263a3bde26af6a7ad2ec00c2cc21
Atilla@Wifi <User@DEV-DESKTOP-00>
2022-04-28 18:52:57 +0200

    WiIP

---
+++ src/20220428/BuchVerwaltung/BuchVerwaltung/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuchVerwaltung
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int DateOfPublish { get; set; }
+        public string Isbn { get; set; }
+        public BookGenre Genre { get; set; }
+    }
+}
---
+++ src/20220428/BuchVerwaltung/BuchVerwaltung/Program.cs
@@ -0,0 +1,63 @@
+﻿using ConsoleTableExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuchVerwaltung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Schreiben Sie eine Applikation mit der Bücher verwaltet werden können.
+             * Die Bücher sollen über einen eigenen Datentyp (struct) abgebildet werden.
+             * Folgende Information über ein Buch sollen verwaltet werden:
+             * 
+             * - Titel
+             * - Autor
+             * - ErscheinungsJahr
+             * - ISBN
+             * - Preis
+             * - Genre
+             * 
+             * Was kann die Applikation:
+             * 
+             * - Bücher einlesen
+             * - Bücher (schön) darstellen
+             *             
+             * */
+
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Title = "Die unendliche Geschichte",
+                    Author = "Michael Ende",
+                    DateOfPublish=1980,
+                    Genre = BookGenre.SienceFiction,
+                    Isbn ="X-4354863215-9",
+                    Price = 32.5m
+                },
+                new Book
+                {
+                    Title ="Die Nadel",
+                    Author = "Ken Follet",
+                    DateOfPublish=1970,
+                    Isbn="X-32154656544-9",
+                    Price = 55.90m,
+                    Genre = BookGenre.Crime
+                }
+            };
+
+            Console.WriteLine("\nMeine Bücher:\n");
+
+            ConsoleTableBuilder.From(books)
+                               .WithFormat(ConsoleTableBuilderFormat.Default)
+                               .ExportAndWriteLine(TableAligntment.Center);
+
+        }
+    }
+}
