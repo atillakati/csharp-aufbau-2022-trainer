96b19ba91aa1a13dc4ef73db90ed32a11556aaeb
Atilla@Wifi <User@DEV-DESKTOP-00>
2022-05-16 20:30:42 +0200

    Beispiel klassische Vererbung - Random Generator

---
+++ src/20220516/RandomStrings/RandomStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace RandomStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new AdvRandom();
+
+            Console.WriteLine(rnd.Next(1, 46));
+            Console.WriteLine(rnd.Next(1, 46));
+
+            Console.WriteLine(rnd.NextDouble()*10.0);
+            Console.WriteLine(rnd.NextDouble() * 10.0);
+
+            Console.WriteLine(rnd.NextString(15));
+            Console.WriteLine(rnd.NextString(10));
+        }
+    }
+}
---
+++ src/20220516/RandomStrings/RandomStrings/RandomExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace RandomStrings
+{
+    public static class RandomExtensions
+    {
+        public static string NextString(this Random rnd, int stringLength)
+        {
+            var randomString = string.Empty;
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                var randomUppercaseChar = (char)rnd.Next('A', 'Z' + 1);
+                var randomLowercaseChar = (char)rnd.Next('a', 'z' + 1);
+                var isUppercase = rnd.Next(0, 2);
+
+                if (isUppercase == 0)
+                {
+                    randomString += randomLowercaseChar;
+                }
+                else
+                {
+                    randomString += randomUppercaseChar;
+                }
+            }
+
+            return randomString;
+        }
+    }
+}
