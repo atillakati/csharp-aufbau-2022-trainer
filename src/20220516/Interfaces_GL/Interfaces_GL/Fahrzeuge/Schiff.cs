873154b4760e8c6863e2d2e348b3d3acccc19fad
Atilla@Wifi <User@DEV-DESKTOP-00>
2022-05-16 20:52:40 +0200

    Interfaces Grundlagen

---
+++ src/20220516/Interfaces_GL/Interfaces_GL/Fahrzeuge/Schiff.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces_GL.Core.Datatypes;
+
+namespace Interfaces_GL.Fahrzeuge
+{
+    internal class Schiff : Fahrzeug
+    {
+        private readonly double _frachtraumGroesseInKubikMeter;
+
+        public Schiff(double frachtraumGroesseInKubikMeter, string model, int yearOfManufacture, Speed maxSpeed)
+            : base(model, yearOfManufacture, maxSpeed)
+        {
+            _frachtraumGroesseInKubikMeter = frachtraumGroesseInKubikMeter;
+        }
+
+        public double FrachtraumGroesseInKubikMeter => _frachtraumGroesseInKubikMeter;
+    }
+}
---
+++ src/20220516/Interfaces_GL/Interfaces_GL/Fahrzeuge/Sportwagen.cs
@@ -0,0 +1,19 @@
+﻿using Interfaces_GL.Core.Datatypes;
+
+namespace Interfaces_GL.Fahrzeuge
+{
+    internal class Sportwagen : Auto
+    {
+        private int _cylinderCount;
+
+        public Sportwagen(string model, int yearOfManufacture, Speed maxSpeed, int cylinderCount)
+            : base(model, yearOfManufacture, maxSpeed, "TopFastHitsOnTheRoad")
+        {
+            _cylinderCount = cylinderCount;
+        }
+
+        public int CylinderCount => _cylinderCount;
+
+
+    }
+}
